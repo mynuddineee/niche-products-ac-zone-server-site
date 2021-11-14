4b4cf24be016faac940e8a5747f4f22b463833a0
mynuddineee <mynuddineee@gmail.com>
2021-11-14 15:54:23 +0600

    third

--- index.js
+++ index.js
@@ -182,7 +182,7 @@ async function run(){
 
   // delete products
 
-  app.delete('/deleteOrder/:id', async(req,res) => {
+  app.delete('/deleteProduct/:id', async(req,res) => {
     const id = req.params.id;
     const query = {_id:ObjectId(id)};
     const results = await productsCollection.deleteOne(query);
